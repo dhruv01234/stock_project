de3996bb592f9178bd08cffb72a23c7a7160ffd5
Dhruv Modi <77389175+dhruv01234@users.noreply.github.com>
2022-07-23 16:37:32 +0530

    some changes

--- stockchart/static/activePage.js
+++
@@ -1,8 +0,0 @@
-const activePage = location.href;
-console.log(activePage);
-const navLinks = document.querySelectorAll('a').
-forEach(link => {
-    if (link.href.includes('${activePage}')) {
-        link.classList.add('active');
-    }
-})
