6f054b18d0697e019657a187581b3e1fef034678
Anmolpandey27 <anmolpandey0275@gmail.com>
2022-07-24 14:59:54 +0530

    some custom styling

--- stockchart/static/activePage.js
+++ stockchart/static/activePage.js
@@ -6,3 +6,15 @@ function fun1() {
     stock = txt1.value;
 }
 btn1.addEventListener('click', fun1);
+
+
+
+  //js
+
+//   $(document).ready(function () {
+  
+//     $("nav-link").click(function (){
+//       $(this).addClass("active").siblings().removeClass("active");
+//     });
+//   });
+  
